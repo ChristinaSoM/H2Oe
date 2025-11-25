f1aa10d87ff96d001a0cf2b155884d5b35faea8f
christinamoser <christina.moser1@stud.fh-campuswien.ac.at>
2025-11-25 16:00:26 +0100

    project structure

---
+++ H2Oe/H2OeTests/H2OeTests.swift
@@ -0,0 +1,17 @@
+//
+//  H2OeTests.swift
+//  H2OeTests
+//
+//  Created by Christina Moser on 25.11.25.
+//
+
+import Testing
+@testable import H2Oe
+
+struct H2OeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
